dfb61014fd3c89460c5bcb4069dc1e8ebd6910fd
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-15 21:56:19 +0300

    Задача №1.2

--- OneDrive/Рабочий стол/Examples/Task011/Program.cs
+++ OneDrive/Рабочий стол/Examples/Task011/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+string numb = Console.ReadLine();
+
+if (numb == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+else if (numb == "2")
+{
+    Console.WriteLine("Вторник");
+}
+else if (numb == "3")
+{
+    Console.WriteLine("Среда");
+}
+else if (numb == "4")
+{
+    Console.WriteLine("Четверг");
+}
+else if (numb == "5")
+{
+    Console.WriteLine("Пятница");
+}
+else if (numb == "6")
+{
+    Console.WriteLine("Суббота");
+}
+else if (numb == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Нет такого дня недели");
+}
