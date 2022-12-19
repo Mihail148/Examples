e5b032049aefc31a95035bd25f936b39a6f623e5
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-19 20:44:59 +0300

    Задача №1

--- OneDrive/Рабочий стол/GeekBrains/Знакомство с языками программирования/Семинар №2/Task1/Program.cs
+++ OneDrive/Рабочий стол/GeekBrains/Знакомство с языками программирования/Семинар №2/Task1/Program.cs
@@ -9,8 +9,8 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+//int firstDigit = number / 10;
+//int secondDigit = number % 10;
 
 //if (firstDigit > secondDigit)
 //{
@@ -20,6 +20,23 @@ int secondDigit = number % 10;
 //{
 //    Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
 //}
- 
-Console.WriteLine($"Наибольшая цифра числа => ");
-Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit); 
+
+//int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+//Console.WriteLine($"Наибольшая цифра числа => ");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+int maxDigit1 = MaxDigit(48);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit1}");
+
+int maxDigit2 = MaxDigit(77);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit2}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
