dc89418fd5f7f4d6685f96003b4ae349594bda1e
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-19 18:15:21 +0300

    Задача №1.1

---
+++ OneDrive/Рабочий стол/GeekBrains/Examples/Task011/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите целое число");
+string numb = Console.ReadLine();
+
+if (numb == "1")
+{
+    Console.WriteLine("Понедельник");
+    Console.WriteLine("Это будний день");
+}
+else if (numb == "2")
+{
+    Console.WriteLine("Вторник");
+    Console.WriteLine("Это будний день");
+}
+else if (numb == "3")
+{
+    Console.WriteLine("Среда");
+    Console.WriteLine("Это будний день");
+}
+else if (numb == "4")
+{
+    Console.WriteLine("Четверг");
+    Console.WriteLine("Это будний день");
+}
+else if (numb == "5")
+{
+    Console.WriteLine("Пятница");
+    Console.WriteLine("Это будний день");
+}
+else if (numb == "6")
+{
+    Console.WriteLine("Суббота");
+    Console.WriteLine("Это выходной день");
+}
+else if (numb == "7")
+{
+    Console.WriteLine("Воскресенье");
+    Console.WriteLine("Это выходной день");
+}
+else 
+{
+    Console.WriteLine("Нет такого дня недели");
+}
