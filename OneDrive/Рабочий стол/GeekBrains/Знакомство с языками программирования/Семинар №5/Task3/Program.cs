9925dd511712e32c6e6847dc2f4f5c149f934ed4
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-16 21:18:10 +0300

    Задача №33

---
+++ OneDrive/Рабочий стол/GeekBrains/Знакомство с языками программирования/Семинар №5/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 33: Задайте массив. Напишите программу, которая
+//определяет, присутствует ли заданное число в массиве.
+
+//4; массив [6, 7, 19, 345, 3] -> нет
+//3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(10, -4, 7);
+PrintArray(array);
+bool ifExist = IfExist(array, number);
+Console.WriteLine(ifExist);
+
+bool IfExist(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
