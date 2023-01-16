ddcee1f96eb24e406ddd520251d68c0d6787575b
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-16 21:19:12 +0300

    Задача №32

---
+++ OneDrive/Рабочий стол/GeekBrains/Знакомство с языками программирования/Семинар №5/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 32: Напишите программу замена элементов
+//массива: положительные элементы замените на
+//соответствующие отрицательные, и наоборот.
+
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+int[] array = CreateArray(10, -50, 50);
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();      
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+void InvertArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * (-1);
+    }
+}
+
+InvertArray(array);
+PrintArray(array);
