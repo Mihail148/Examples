3f656f010dc18a7a11cf5e28be00ee7a0075dd18
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-26 21:07:59 +0300

    Задача №55

--- OneDrive/Рабочий стол/GeekBrains/Знакомство с языками программирования/Семинар №8/Task2/Program.cs
+++ OneDrive/Рабочий стол/GeekBrains/Знакомство с языками программирования/Семинар №8/Task2/Program.cs
@@ -50,9 +50,9 @@ void PrintMatrix(int[,] matrix)
 
 void ChangeRowColumnsMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
     {
-        for (int j = i; j < matrix.GetLength(1); j++)
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
         {
             int temp = matrix[i, j];
             matrix[i, j] = matrix[j, i];
