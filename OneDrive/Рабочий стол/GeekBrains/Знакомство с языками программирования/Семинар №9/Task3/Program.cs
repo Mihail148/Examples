7c9635b955a72f35c5c370ea7b652af2d28cefba
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-30 21:37:00 +0300

    Задача №67

---
+++ OneDrive/Рабочий стол/GeekBrains/Знакомство с языками программирования/Семинар №9/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и
+//возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+//int num = number % 10;
+//number = number / 10;
+//sum = sum + num;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine($"{sumDigit}");
+
+int SumDigit (int n)
+{
+    if(n == 0) return 0;
+    return n % 10 + SumDigit(n / 10);
+}
